a107108eea012f3cf6c0360a196d5cdfb919cdde
cinallegam <cinallegam@gmail.com>
2025-08-03 12:01:25 +0700

    add tokenVerify on path /users

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', [tokenVerify], usersRouter);
 app.use('/shops', shopsRouter);
 
 app.use('/auth', loginRouter);
